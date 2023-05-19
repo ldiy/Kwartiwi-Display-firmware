8cd02fd0fe55c639bda83d6ea29296ae986697cc
L-diy <ldiy@users.noreply.github.com>
2023-05-20 00:21:14 +0200

    Added server-discovery, and using/saving server config from NVS to the webclient

--- main/include/web_client.h
+++ main/include/web_client.h
@@ -3,17 +3,36 @@
 
 #include "esp_event.h"
 
+#define WEB_CLIENT_NVS_NAMESPACE "web_client"
+#define WEB_CLIENT_NVS_SERVER_HOSTNAME_KEY "srv-host"
+
 ESP_EVENT_DECLARE_BASE(WEB_CLIENT_EVENTS);
 
 typedef enum {
     WEB_CLIENT_INITIALIZED,
     WEB_CLIENT_EVENT_CONNECTED,
     WEB_CLIENT_EVENT_DISCONNECTED,
+    WEB_CLIENT_SERVERS_FOUND,
 //    WEB_CLIENT_EVENT_METER_DATA_RECEIVED,
 //    WEB_CLIENT_EVENT_METER_HISTORY_RECEIVED,
 } web_client_event_id_t;
 
+typedef struct {
+    char hostname[256];
+    uint16_t port;
+    esp_ip_addr_t ip;
+} web_client_server_t;
+
+// servers found cb
+typedef void (*web_client_servers_found_cb_t)(web_client_server_t *servers, uint8_t count);
+
+
 // Function prototypes
 _Noreturn void web_client_task(void *pvParameters);
+void web_client_find_servers(void);
+
+uint8_t web_client_get_found_servers(web_client_server_t **servers);
+SemaphoreHandle_t web_client_get_found_servers_mutex(void);
+esp_err_t web_client_save_server_config(const char *hostname);
 
 #endif //WEB_CLIENT_H
--- main/web_client.c
+++ main/web_client.c
@@ -2,7 +2,10 @@
  * @file web_client.c
  * @brief Web client task and related functions
  *
- * This task periodically requests meter data from the API and publishes it to the data manager.
+ * This file contains a task that periodically sends requests to the server and parses the response.
+ * The parsed data is then sent to the data manager.
+ *
+ * This file also contains functions for server discovery using mDNS, and setting the server configuration.
  */
 
 #include <stdint.h>
@@ -12,13 +15,16 @@
 #include <math.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "esp_log.h"
-#include "nvs_flash.h"
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_http_client.h"
+#include "nvs.h"
+#include "mdns.h"
 #include "cJSON.h"
 #include "data_manager.h"
+#include "networking.h"
 #include "web_client.h"
 
 #define DISCONNECTED_STATUS_FAILED_REQ_COUNT 5  // Number of consecutive failed requests before setting the status to disconnected
@@ -27,9 +33,12 @@
 #define API_METER_DATA_ENDPOINT "/api/meter-data"
 #define API_METER_DATA_HISTORY_ENDPOINT "/api/meter-data-history"
 
+#define SERVER_DISCOVERY_TIMEOUT_MS 5000
+#define SERVER_DISCOVERY_MAX_SERVERS 5
+
 ESP_EVENT_DEFINE_BASE(WEB_CLIENT_EVENTS);
 
-extern esp_event_loop_handle_t loop_handle;
+extern esp_event_loop_handle_t app_loop_handle;
 
 // parse function callback type
 typedef esp_err_t (*parse_publish_data_cb_t)(uint8_t *buf, uint32_t len);
@@ -38,6 +47,11 @@ static const char *TAG = "web_client";
 static uint8_t *http_buf = NULL;
 static esp_http_client_config_t config;
 static bool connected = false;
+static char server_host[256];
+
+static web_client_server_t found_servers[SERVER_DISCOVERY_MAX_SERVERS];
+static uint8_t found_servers_count = 0;
+SemaphoreHandle_t found_servers_mutex = NULL; // Mutex for found_servers and found_servers_count
 
 // Function prototypes
 static esp_err_t web_client_init(void);
@@ -45,6 +59,8 @@ static esp_err_t http_event_handler(esp_http_client_event_t *e);
 static esp_err_t parse_publish_meter_data(uint8_t *buf, uint32_t len);
 static esp_err_t parse_publish_meter_data_history(uint8_t *buf, uint32_t len);
 static esp_err_t request(const char *path, parse_publish_data_cb_t cb);
+static esp_err_t read_server_config_from_nvs(void);
+
 
 /**
  * @brief Web client task
@@ -54,7 +70,7 @@ static esp_err_t request(const char *path, parse_publish_data_cb_t cb);
  *
  * @note This task should only be started after the following components have been initialized:
  *   - Networking
- *   - Event loop
+ *   - Event loop (default and app)
  *   - Data manager
  *
  * @todo Keep the connection open as long as possible to reduce overhead
@@ -74,8 +90,7 @@ _Noreturn void web_client_task(void *pvParameters) {
         vTaskDelete(NULL);
         abort();
     }
-
-    ESP_ERROR_CHECK(esp_event_post_to(loop_handle, WEB_CLIENT_EVENTS, WEB_CLIENT_INITIALIZED, NULL, 0, portMAX_DELAY));
+    ESP_ERROR_CHECK(esp_event_post_to(app_loop_handle, WEB_CLIENT_EVENTS, WEB_CLIENT_INITIALIZED, NULL, 0, portMAX_DELAY));
 
     // Request meter data history
     while (request(API_METER_DATA_HISTORY_ENDPOINT, parse_publish_meter_data_history) != ESP_OK) {
@@ -100,6 +115,183 @@ _Noreturn void web_client_task(void *pvParameters) {
 }
 
 /**
+ * @brief Task that searches for servers using mDNS
+ *
+ * This task searches for servers using mDNS and stores the results in found_servers.
+ * When the task is finished, it posts the WEB_CLIENT_SERVERS_FOUND event to the default event loop.
+ *
+ * @param[in] pvParameters unused
+ */
+void find_servers_task(void *pvParameters) {
+    ESP_LOGI(TAG, "Starting find servers task");
+
+    mdns_result_t * results = NULL;
+    uint8_t num_servers = 0;
+
+    esp_err_t err = mdns_query_ptr("_kwartiwi-p1", "_tcp", SERVER_DISCOVERY_TIMEOUT_MS, SERVER_DISCOVERY_MAX_SERVERS, &results);
+    if(err){
+        ESP_LOGE(TAG, "Query Failed");
+        return;
+    }
+    if(!results){
+        ESP_LOGW(TAG, "No results found!");
+        return;
+    }
+
+    // Take the mutex
+    xSemaphoreTake(found_servers_mutex, portMAX_DELAY);
+
+    if (err == ESP_OK && results != NULL) {
+        for (mdns_result_t * r = results; r; r = r->next) {
+            if (r->hostname) {
+                // store the hostname with .local added
+//                strncpy(found_servers[num_servers].hostname, r->hostname, sizeof(found_servers[num_servers].hostname));
+                snprintf(found_servers[num_servers].hostname, sizeof(found_servers[num_servers].hostname), "%s.local", r->hostname);
+            }
+            if (r->port) {
+                // store the port
+                found_servers[num_servers].port = r->port;
+            }
+            if (r->addr) {
+                // store the ip address
+                found_servers[num_servers].ip = r->addr->addr;
+            }
+            ESP_LOGD(TAG, "Found server %s:%d", found_servers[num_servers].hostname, found_servers[num_servers].port);
+            num_servers++;
+        }
+    }
+    found_servers_count = num_servers;
+    mdns_query_results_free(results);
+
+    ESP_LOGI(TAG, "Found %d servers", num_servers);
+
+    // Give the mutex back
+    xSemaphoreGive(found_servers_mutex);
+
+    // Notify the rest of the application that servers have been found
+    ESP_ERROR_CHECK(esp_event_post_to(app_loop_handle, WEB_CLIENT_EVENTS, WEB_CLIENT_SERVERS_FOUND, NULL, 0, portMAX_DELAY));
+
+    vTaskDelete(NULL);
+}
+
+/**
+ * @brief Starts the find servers task
+ *
+ * This function starts the find servers task. When the task is finished, it posts the WEB_CLIENT_SERVERS_FOUND event.
+ * The found servers can be accessed using web_client_get_found_servers().
+ *
+ * The task is started with the same priority as the task that calls this function.
+ */
+void web_client_find_servers(void) {
+
+   if (found_servers_mutex == NULL) {
+        found_servers_mutex = xSemaphoreCreateMutex();
+        assert(found_servers_mutex != NULL);
+    }
+
+    // Start the task with the same priority as the current task
+    xTaskCreate(find_servers_task, "find_servers_task", 4096, NULL, uxTaskPriorityGet(NULL), NULL);
+}
+
+/**
+ * @brief Get the found servers
+ *
+ *
+ * @note The found servers mutex must be taken before calling this function or using the returned pointer
+ *
+ * @param[out] servers A pointer to an array of web_client_server_t structs, this will be set to the found servers pointer
+ * @return The number of found servers
+ */
+uint8_t web_client_get_found_servers(web_client_server_t **servers) {
+    uint8_t num_servers = 0;
+
+    num_servers = found_servers_count;
+    *servers = found_servers;
+
+    return num_servers;
+}
+
+/**
+ * @brief Get the found servers mutex
+ *
+ * @warnig web_client_find_servers() must have been called once before calling this function, otherwise the mutex may not be created yet
+ *
+ * @return The found servers mutex
+ */
+SemaphoreHandle_t web_client_get_found_servers_mutex(void) {
+    assert(found_servers_mutex != NULL);
+    return found_servers_mutex;
+}
+
+/**
+ * @brief Save the given server config to NVS
+ *
+ * @param[in] hostname The hostname of the server to save
+ * @return ESP_OK on success, otherwise an NVS error code
+ */
+esp_err_t web_client_save_server_config(const char *const hostname) {
+    nvs_handle_t nvs_handle;
+    esp_err_t err = nvs_open(WEB_CLIENT_NVS_NAMESPACE, NVS_READWRITE, &nvs_handle);
+
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to open NVS namespace %s", WEB_CLIENT_NVS_NAMESPACE);
+        return err;
+    }
+
+    err = nvs_set_str(nvs_handle, WEB_CLIENT_NVS_SERVER_HOSTNAME_KEY, hostname);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to set %s in NVS", WEB_CLIENT_NVS_SERVER_HOSTNAME_KEY);
+        nvs_close(nvs_handle);
+        return err;
+    }
+
+    err = nvs_commit(nvs_handle);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to commit NVS");
+        nvs_close(nvs_handle);
+        return err;
+    }
+
+    nvs_close(nvs_handle);
+
+    return ESP_OK;
+}
+
+/**
+ * @brief Read the server config from NVS
+ *
+ * @note This function uses the global server_host variable
+ * @warnig This function is not thread safe, it should only be called from the web client task
+ *
+ * @return ESP_OK on success, otherwise an NVS error code
+ */
+static esp_err_t read_server_config_from_nvs(void) {
+    nvs_handle_t nvs_handle;
+    esp_err_t err;
+
+    // Open NVS
+    err = nvs_open(WEB_CLIENT_NVS_NAMESPACE, NVS_READONLY, &nvs_handle);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to open NVS");
+        return ESP_FAIL;
+    }
+
+    // Read the server URL
+    size_t host_len = sizeof(server_host);
+    err = nvs_get_str(nvs_handle, WEB_CLIENT_NVS_SERVER_HOSTNAME_KEY, server_host, &host_len);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to read server URL from NVS");
+        return ESP_FAIL;
+    }
+
+    // Close NVS
+    nvs_close(nvs_handle);
+
+    return ESP_OK;
+}
+
+
+/**
  * @brief Initialize the web client
  *
  * @note This function should only be called once, and only from the web client task
@@ -113,9 +305,12 @@ static esp_err_t web_client_init(void) {
         return ESP_ERR_NO_MEM;
     }
 
+    // Read config from NVS
+    ESP_ERROR_CHECK(read_server_config_from_nvs());
+
     config.user_data = http_buf;
     config.event_handler = http_event_handler;
-    config.host = "10.10.10.31"; // TODO: get from NVS or mDNS
+    config.host = server_host;
     config.path = "/";
 
     return ESP_OK;
@@ -178,7 +373,7 @@ static esp_err_t request(const char *path, parse_publish_data_cb_t cb) {
             ESP_LOGI(TAG, "Setting status to connected");
             failed_req_count = 0;
             connected = true;
-            ESP_ERROR_CHECK(esp_event_post_to(loop_handle, WEB_CLIENT_EVENTS, WEB_CLIENT_EVENT_CONNECTED, NULL, 0, portMAX_DELAY));
+            ESP_ERROR_CHECK(esp_event_post_to(app_loop_handle, WEB_CLIENT_EVENTS, WEB_CLIENT_EVENT_CONNECTED, NULL, 0, portMAX_DELAY));
         }
     }
     else {
@@ -187,7 +382,7 @@ static esp_err_t request(const char *path, parse_publish_data_cb_t cb) {
         if (failed_req_count == DISCONNECTED_STATUS_FAILED_REQ_COUNT) {
             ESP_LOGE(TAG, "Maximum failed request count reached. Setting status to disconnected");
             connected = false;
-            ESP_ERROR_CHECK(esp_event_post_to(loop_handle, WEB_CLIENT_EVENTS, WEB_CLIENT_EVENT_DISCONNECTED, NULL, 0, portMAX_DELAY));
+            ESP_ERROR_CHECK(esp_event_post_to(app_loop_handle, WEB_CLIENT_EVENTS, WEB_CLIENT_EVENT_DISCONNECTED, NULL, 0, portMAX_DELAY));
         }
     }
 
